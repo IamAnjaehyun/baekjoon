b92f8925b840094e6a0c2c83b5a69d1a82dc7785
IamJaehyun <lvet0330@gmail.com>
2022-09-11 21:41:48 +0900

    third commit

---
+++ baekjoonPY/1271.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 11 21:30:05 2022
+
+@author: ANJH
+"""
+
+A ,B = map(int, input().split())
+print(int(A//B))
+print(int(A%B))
---
+++ baekjoonPY/2338.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 11 21:20:18 2022
+
+@author: ANJH
+"""
+def calculator(A,B):
+    print(A+B)
+    print(A-B)
+    print(A*B)
+
+
+A = int(input())
+B = int(input())
+
+calculator(A, B)
