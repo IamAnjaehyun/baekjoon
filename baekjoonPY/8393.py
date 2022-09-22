568a128afd97c3c1a7230d062b2e773238fc0370
IamJaehyun <lvet0330@gmail.com>
2022-09-22 14:40:18 +0900

    220922

---
+++ baekjoonPY/8393.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 22 14:25:03 2022
+
+@author: ANJH
+"""
+n = int(input())
+
+total = 0  
+for i in range(1, n+1) :
+    total += i
+print(total)
---
+++ baekjoonPY/9086.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 22 14:33:44 2022
+
+@author: ANJH
+"""
+
+i = int(input())
+
+for i in range(1, i+1):
+    
+    text = input()
+    print(text[0]+text[-1])
+    
+    
+        
+        
+    
