e77c377810c2b6e419b27e4e07a550dd768db4fd
IamJaehyun <lvet0330@gmail.com>
2022-09-24 23:55:49 +0900

    220924

---
+++ baekjoonPY/9654.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 24 11:31:50 2022
+
+@author: ANJH
+"""
+
+print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE")
+print("N2 Bomber      Heavy Fighter  Limited    21        ")
+print("J-Type 327     Light Combat   Unlimited  1         ")
+print("NX Cruiser     Medium Fighter Limited    18        ")
+print("N1 Starfighter Medium Fighter Unlimited  25        "_
+print("Royal Cruiser  Light Combat   Limited    4         ")
