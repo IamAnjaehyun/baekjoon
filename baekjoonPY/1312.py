d442eceb1c426b55a565077d37ce154a66a9f569
IamJaehyun <lvet0330@gmail.com>
2022-10-08 16:20:32 +0900

    221008

---
+++ baekjoonPY/1312.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  8 16:13:00 2022
+
+@author: ANJH
+"""
+
+a,b,c = map(int, input().split())
+
+answer = 0
+for i in range(c+1):
+    answer = a//b
+    a = a%b * 10
+print(answer)
