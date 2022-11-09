bf3c9c43c3f4070725e8de3db95f9a6ca6e2463f
Anjaehyun <101981639+IamAnjaehyun@users.noreply.github.com>
2022-11-09 17:53:20 +0900

    # 1402
    이게무슨..

---
+++ baekjoonPY/1402.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  9 17:49:30 2022
+
+@author: ANJH
+"""
+
+T = int(input())
+
+for i in range(T):
+    A, B = map(int, input().split())
+    print('yes')
