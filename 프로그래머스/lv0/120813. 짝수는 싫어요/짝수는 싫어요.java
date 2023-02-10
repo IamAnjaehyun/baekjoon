43a50b934efed9e67cdfe447c7e3bb71762c176a
Anjaehyun <lvet0330@gmail.com>
2023-02-10 18:09:53 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public ArrayList solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        for(int i=0;i<=n;i++){
+            if(i%2!=0){answer.add(i);}
+        }
+        
+        
+        return answer;
+    }
+}
