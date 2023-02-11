dbd0214cd77242e874a4b084e44f0d82d0d8eb13
Anjaehyun <lvet0330@gmail.com>
2023-02-11 17:37:17 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        for(int x : numbers){
+            answer += x;
+        }
+        return answer/numbers.length;
+    }
+}
