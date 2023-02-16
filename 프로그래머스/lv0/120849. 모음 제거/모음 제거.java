f3135f48f2abea37be524b220c630cff2fdee018
Anjaehyun <lvet0330@gmail.com>
2023-02-16 15:33:14 +0900

    [level 0] Title: 모음 제거, Time: 14.83 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        char[] ans = new char[my_string.length()];
+
+        for (int i = 0; i < my_string.length(); i++) {
+            ans[i] = my_string.charAt(i);
+        }
+
+        for (char s : ans) {
+            if (s != 'a' && s != 'e' && s != 'i' && s != 'o' && s != 'u') {
+                answer += s;
+            }
+        }
+        return answer;
+    }
+}
