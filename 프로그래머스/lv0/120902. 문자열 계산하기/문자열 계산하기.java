cafd80bf13f1de588920b893eca6a467a711c5e4
Anjaehyun <lvet0330@gmail.com>
2023-02-18 19:13:10 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.09 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(String my_string) {
-            int answer = 0;
+        int answer = 0;
         String[] arr = my_string.split(" ");
 
         for (int i = 0; i < arr.length; i++) {
