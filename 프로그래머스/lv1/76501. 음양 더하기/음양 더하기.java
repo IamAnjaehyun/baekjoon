9325b036994a2097eb2e50ac1e01452213c194f4
Anjaehyun <lvet0330@gmail.com>
2023-02-28 15:53:06 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 89.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if(!signs[i]){
+                absolutes[i] = -absolutes[i];
+            }
+        }
+        for (int a : absolutes){
+            answer+=a;
+        }
+
+        return answer;
+    
+    }
+}
