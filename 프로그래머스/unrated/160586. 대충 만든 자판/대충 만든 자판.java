77f1e63f06873bd946f342131276ba77e441c78c
Anjaehyun <lvet0330@gmail.com>
2023-03-01 18:51:25 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.87 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,28 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        int[] answer = new int[targets.length];
-        Map<Character, Integer> keyBoard = new HashMap<>();
-        for(String k:keymap){
-            for(int i=0;i<k.length();i++){
-                char sign = k.charAt(i);
-                int step=keyBoard.getOrDefault(sign,i+1);
-                keyBoard.put(sign,Math.min(step,i+1));
-            }
-        } //알파벳 몇번가야되는지 저장
-
-        for(int i=0;i<targets.length;i++){
-            for(int j=0;j<targets[i].length();j++){
-                char c = targets[i].charAt(j);
-                if(keyBoard.containsKey(c)){
-                    answer[i]+=keyBoard.get(c);
-                }else{
-                    answer[i]=-1;
-                    break;
-                }
-            }
-        }
+        int[] answer = {};
         return answer;
     }
 }
