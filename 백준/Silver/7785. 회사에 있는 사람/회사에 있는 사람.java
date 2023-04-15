2140a244e4b20d2330658f51f7d0c73577ca84d0
Anjaehyun <lvet0330@gmail.com>
2023-04-15 21:23:17 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1432 ms, Memory: 54424 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -7,28 +7,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        Map<String, Integer> map = new TreeMap<>();
-
-        for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split(" ");
-            String name = input[0];
-            String status = input[1];
-
-            if (status.equals("enter")) {
-                map.put(name, 1);
-            } else {
-                map.remove(name);
+        
+        int N = Integer.parseInt(br.readLine());
+        Map<String, String> map = new HashMap<>();
+        StringTokenizer st;
+        //HashMap으로 떠난사람은 leave로 갱신 
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
+        }
+        //퇴근 안한사람은 value = enter
+        List<String> names = new ArrayList<>();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (entry.getValue().equals("enter")) {
+                names.add(entry.getKey());
             }
         }
-        
-        List<String> names = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            names.add(entry.getKey());
-        }
+        //사전 역순 정렬
         names.sort(Comparator.reverseOrder());
-        for(String name : names){
+        //출력
+        for (String name : names) {
             System.out.println(name);
         }
     }
