b994fb674f19e97069ba26b813f5e807ed7c119d
Anjaehyun <lvet0330@gmail.com>
2023-06-15 23:07:00 +0900

    [Silver V] Title: 국회의원 선거, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    
+    public static void main(String [] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+                
+        final int NUMBER_OF_CANDI = Integer.parseInt(br.readLine());
+        int dasom = Integer.parseInt(br.readLine());
+       
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<NUMBER_OF_CANDI-1;i++) {
+            priorityQueue.add(Integer.parseInt(br.readLine()));
+        }
+        int answer = 0;
+        if(NUMBER_OF_CANDI != 1) {
+            while(priorityQueue.peek() >= dasom) {
+                answer++;
+                dasom++;
+                int temp = priorityQueue.poll();
+                priorityQueue.add(temp-1);
+            
+            } 
+        }
+       
+        
+        sb.append(answer);
+        sb.append("\n"); 
+        
+        bw.write(sb.toString());
+        
+        bw.flush();
+        br.close();
+        bw.close();
+        
+    }
+
+    
+}
